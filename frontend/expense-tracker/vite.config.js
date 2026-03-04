05992321546bed3ac190bb84845c8c9c72bcc8a2
Assumpta Ozor <assumptaozor@Assumptas-MacBook-Air.local>
2026-03-04 10:56:21 -0800

    Full MERN Expense Tracker App

--- frontend/expense-tracker/vite.config.js
+++ frontend/expense-tracker/vite.config.js
@@ -8,11 +8,26 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
+
+
+
+// import { defineConfig } from 'vite'
 // import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(),],
-})
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
 
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
