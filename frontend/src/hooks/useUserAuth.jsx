d53e00a8d7488c1b979cd0626edc6409d533517a
Assumpta Ozor <assumptaozor@Assumptas-MacBook-Air.local>
2026-03-04 22:26:56 -0800

    fix useUserAuth response undefined

--- frontend/src/hooks/useUserAuth.jsx
+++ frontend/src/hooks/useUserAuth.jsx
@@ -1,41 +1,21 @@
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/UserContext"
-import axiosInstance from "../utils/axiosInstance";
-import { API_PATHS } from "../utils/apiPaths";
+import { UserContext } from "../context/UserContext";
 import { useContext, useEffect } from "react";
 
-export const useUserAuth = () =>{
-    const {user, updateUser, clearUser} = useContext(UserContext);
-    const navigate = useNavigate();
+export const useUserAuth = () => {
+  const { user, updateUser, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(user) return;
+  useEffect(() => {
+    if (user) return;
 
-        let isMounted = true;
+    const storedUser = localStorage.getItem("user");
 
-        const fetchUserInfo = async () => {
-            try {
-                // const response = await axiosInstance.get(API_PATHS.AUTH.GET_USER_INFO);
-
-                if(isMounted && response.data){
-                    updateUser(response.data);
-                }
-            } catch (error) {
-                console.log("Failed to fetch user info", error);
-
-                if(isMounted){
-                    clearUser();
-                    navigate("/login");
-                }
-            }
-        };
-
-        fetchUserInfo();
-
-        return () =>{
-            isMounted = false;
-
-
-        };
-    }, [updateUser, clearUser, navigate])
+    if (storedUser) {
+      updateUser(JSON.parse(storedUser));
+    } else {
+      clearUser();
+      navigate("/login");
+    }
+  }, [user, updateUser, clearUser, navigate]);
 };
