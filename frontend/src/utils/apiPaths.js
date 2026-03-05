b92a914058ee03ab4a58442bf6a1cd626bb68b7f
Assumpta Ozor <assumptaozor@Assumptas-MacBook-Air.local>
2026-03-04 19:23:22 -0800

    fix duplicate api path

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -6,8 +6,8 @@ export const BASE_URL = "https://expense-tracker-2-tx8p.onrender.com/api/v1";
 
 export const API_PATHS = {
     AUTH: {
-        LOGIN:"/api/v1/auth/login",
-        REGISTER:"/api/v1/auth/register",
+        LOGIN:"/auth/login",
+        REGISTER:"/auth/register",
         GET_USER_INFO:"/api/v1/auth/getUser",
     },
 
