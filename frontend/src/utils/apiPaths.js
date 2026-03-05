e97800465fe0d8297b7133680de79485a6661ec8
Assumpta Ozor <assumptaozor@Assumptas-MacBook-Air.local>
2026-03-04 19:45:02 -0800

    fix api paths

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,6 +1,5 @@
-// export const BASE_URL = "http://localhost:8000/api/v1";
-
-export const BASE_URL = "https://expense-tracker-2-tx8p.onrender.com/api/v1";
+export const BASE_URL =
+  "https://expense-tracker-2-tx8p.onrender.com/api/v1";
 
 export const API_PATHS = {
   AUTH: {
